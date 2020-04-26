447130281a22d2d5898cb92776b5d5c1e0f0f361
Keron320 <32654913+Keron320@users.noreply.github.com>
2020-04-26 22:06:16 +0300

    Initial commit

---
+++ Test1.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test1.aspx.cs" Inherits="test.Test1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <style>
+        html {
+            background-color: silver;
+        }
+        .tabs {
+            position: relative;
+            top: 1px;
+            left: 10px;
+        }
+        .tab{
+            border:solid 1px black;
+            background-color:dimgray;
+            padding:5px 10px;
+        }
+        .selectedTab
+        {
+            background-color:white;
+            border-bottom:solid 1px white;
+        }
+        .tabContents{
+            border: solid 1px black;
+            padding: 10px;
+            background-color:white;
+        }
+    </style>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Menu ID="Menu1" Orientation ="Horizontal"  StaticMenuItemStyle-CssClass="tab" Font-Size ="Large"
+                StaticSelectedStyle-CssClass="selectedTab" StaticMenuItemStyle-HorizontalPadding="50px"
+                StaticSelectedStyle-BackColor ="White" CssClass="tabs" runat="server" OnMenuItemClick="Menu1_MenuItemClick">
+            <Items>
+                <asp:MenuItem Text="Product list" Value="0" Selected="true"></asp:MenuItem>
+                <asp:MenuItem Text="Product details" Value="1"></asp:MenuItem>
+            </Items>
+            </asp:Menu>
+
+            <div class="tabContents">
+                <asp:Multiview ID="Multiview1" ActiveViewIndex="0" runat="server">
+                    <asp:View ID="View1" runat="server">
+                        <asp:DropDownList ID="SortList" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                        </asp:DropDownList>
+                        <br />
+                        <br />
+                        <asp:Panel ID="MasterPanel" runat="server" BorderStyle="Inset" Height="392px" Width="666px" HorizontalAlign="Left" ScrollBars="Auto">
+                        </asp:Panel>
+                        <br />
+                        <br />
+                        <br />
+                    </asp:View>
+                    <asp:View ID="View2" runat="server">
+                        Product Details
+                        <asp:Panel ID="Panel3" runat="server" BorderStyle="Inset" Height="343px" Width="536px">
+                            <asp:Image ID="D_ProdImg1" runat="server" Height="64px" ImageAlign="Left" Width="64px" style="margin-top: 33px" />
+                            <asp:Label ID="D_ProdTitle" runat="server" Text="Product Title"></asp:Label>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <asp:Panel ID="D_Prod_Description" runat="server" Height="86px" Width="485px">
+                                Product Description:Description<br />
+                                <asp:Label ID="D_Price" runat="server" Text="Price:"></asp:Label>
+                            </asp:Panel>
+                            <asp:Panel ID="Panel5" runat="server" Height="164px">
+                                Specifications<br />
+                                <br />
+                                <asp:Label ID="D_spec1" runat="server" Text="Label"></asp:Label>
+                                <br />
+                                <asp:Label ID="D_spec2" runat="server" Text="Label"></asp:Label>
+                                <br />
+                                <asp:Label ID="D_spec3" runat="server" Text="Label"></asp:Label>
+                                <br />
+                                <asp:Label ID="D_spec4" runat="server" Text="Label"></asp:Label>
+                            </asp:Panel>
+                        </asp:Panel>
+
+                    </asp:View>
+                    </asp:Multiview>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Test1.aspx.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace test
+{
+    public partial class Test1 : System.Web.UI.Page
+    {
+
+        public Panel ItemPanel;
+        public Label ProductPrice;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.BindGrid();
+            this.GenerateList();
+            this.GenerateDetail();
+        }
+
+        protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
+        {
+                int index = Int32.Parse(e.Item.Value);
+                Multiview1.ActiveViewIndex = index;
+        }
+
+        private void BindGrid()
+        {
+            using (DataSet ds = new DataSet())
+            {
+                ds.ReadXml(Server.MapPath("~/XML/List.xml"));
+                //GridView1.DataSource = ds;
+                //GridView1.DataBind();
+            }
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GenerateList()
+        {
+            Panel myPanel = new Panel();
+
+            
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath("~/XML/List.xml"));
+
+
+
+            XmlNodeList elemList = xmlDoc.GetElementsByTagName("Product");
+            //XmlNodeList detailElemList = detailDoc.GetElementsByTagName("Specs");
+
+            int index = 0;
+            foreach (XmlNode node in elemList)
+            {
+                    ItemPanel = new Panel();
+                    MasterPanel.Controls.Add(ItemPanel);
+                    ItemPanel.Height = 90;
+                    ItemPanel.Width = 600;
+                    ItemPanel.BorderStyle = BorderStyle.Inset;
+
+                    Image ProductImage = new Image();
+                    ItemPanel.Controls.Add(ProductImage);
+                    ProductImage.Height = 64;
+                    ProductImage.Width = 64;
+                    ProductImage.ImageAlign = ImageAlign.Left;
+                    ProductImage.ImageUrl = node["Image"].InnerText;
+
+                    Label ProductTitle = new Label();
+                    ItemPanel.Controls.Add(ProductTitle);
+                    ProductTitle.Text = node.Attributes[0].InnerText;
+
+
+                    Label TextPrice = new Label();
+                    ItemPanel.Controls.Add(TextPrice);
+                    TextPrice.Text = Environment.NewLine + "Price: ";
+
+                    ProductPrice = new Label();
+                    ItemPanel.Controls.Add(ProductPrice);
+                    ProductPrice.Text = node["Price"].InnerText;
+
+                    Label ProductDescription = new Label();
+                    ItemPanel.Controls.Add(ProductDescription);
+                    ProductDescription.Text = node["Description"].InnerText;
+
+                    Label ProductAvailability = new Label();
+                    ItemPanel.Controls.Add(ProductAvailability);
+                
+                    Button Btn_MoreDetails = new Button();
+                    ItemPanel.Controls.Add(Btn_MoreDetails);
+                    Btn_MoreDetails.Text = "More Details";
+                    Btn_MoreDetails.ID = "Button" + index.ToString();
+                    Btn_MoreDetails.Click += new EventHandler(InfoClick);
+                index++;
+
+            }
+
+      
+           
+        }
+
+
+        protected void GenerateDetail()
+        {
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath("~/XML/List.xml"));
+
+            XmlDocument detailDoc = new XmlDocument();
+            detailDoc.Load(Server.MapPath("~/XML/Detail.xml"));
+
+            //XmlNodeList elemList = xmlDoc.GetElementsByTagName("Product");
+
+        }
+
+
+
+
+
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void InfoClick(object sender, EventArgs e)
+        {
+            Multiview1.ActiveViewIndex = 1;
+
+           
+            XmlDocument detailDoc = new XmlDocument();
+            detailDoc.Load(Server.MapPath("~/XML/Detail.xml"));
+
+
+            XmlNodeList detailElemList = detailDoc.GetElementsByTagName("Product");
+            //XmlNode DetailNode = detailDoc;
+
+           
+            foreach (XmlNode DetailNode in detailElemList)
+            {
+                D_ProdTitle.Text = DetailNode.Attributes[0].InnerText;
+                D_Prod_Description.GroupingText = DetailNode["Description"].InnerText;
+                D_ProdImg1.ImageUrl = DetailNode["Image"].InnerText;
+                D_Price.Text = "999";
+                D_spec1.Text = "Test";
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click2(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
